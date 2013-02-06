a054932744a4e440c5c0b04bc55b64374c64e7a3
Patrick Scott <patrick.lee.scott@gmail.com>
2013-02-06 16:25:41 -0500

    experimenting with refactoring/architecture

---
+++ domain/Posts.js
@@ -0,0 +1,17 @@
+Posts = new Meteor.Collection("posts");
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Posts.find().count() > 0) return;
+
+    for (var i = 0; i < 4; i++) {
+      Posts.insert({
+        title: "Post " + i
+      });
+    }
+  });
+
+  Meteor.publish("posts", function () {
+    return Posts.find();
+  });
+}
