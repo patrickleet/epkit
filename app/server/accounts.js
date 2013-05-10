379a75de60d37a8b963cdf341f13aca36e95dbc0
Patrick Scott <patrick.lee.scott@gmail.com>
2013-05-09 23:13:11 -0400

    update fixtures on meteor start-up

--- app/server/accounts.js
+++ app/server/accounts.js
@@ -1,31 +1,33 @@
-if (Meteor.users.find().count() == 0) {
-  var users = [
-    {
-      name: "Patrick Scott",
-      email:"patrick.lee.scott@gmail.com",
-      roles:['admin']
-    },
-    {
-      name: "Krish Jargidar",
-      email:"krishjargidar@gmail.com",
-      roles:['user']}
-  ];
+Meteor.startup(function () {
+  if (Meteor.users.find().count() == 0) {
+    var users = [
+      {
+        name: "Patrick Scott",
+        email:"patrick.lee.scott@gmail.com",
+        roles:['admin']
+      },
+      {
+        name: "Krish Jargidar",
+        email:"krishjargidar@gmail.com",
+        roles:['user']}
+    ];
 
-  _.each(users, function (user) {
-    var id;
+    _.each(users, function (user) {
+      var id;
 
-    id = Accounts.createUser({
-      email: user.email,
-      profile: { name: user.name }
-    });
+      id = Accounts.createUser({
+        email: user.email,
+        profile: { name: user.name }
+      });
 
-    if (user.roles.length > 0) {
-      Roles.addUsersToRoles(id, user.roles);
-      if (user.roles.indexOf('admin') >= 0) {
-        Accounts.sendEnrollmentEmail(id, user.email)
+      if (user.roles.length > 0) {
+        Roles.addUsersToRoles(id, user.roles);
+        if (user.roles.indexOf('admin') >= 0) {
+          Accounts.sendEnrollmentEmail(id, user.email)
+        }
       }
-    }
 
 
-  });
-}
+    });
+  }
+});
